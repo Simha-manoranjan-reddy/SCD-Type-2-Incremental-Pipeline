728d5f49ef631775faed513fabc015d721f0b728
Karedla Simhamanoranjan Reddy <100258235+Simha-manoranjan-reddy@users.noreply.github.com>
2026-06-13 16:03:55 +0530

    Add initial customer data to SourceCustomer
    Insert multiple customer records into SourceCustomer table.

---
+++ SQL Queries/02_insert_day1_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO dbo.SourceCustomer
+VALUES
+(101, 'Ravi Kumar', 'Hyderabad', 'ravi.kumar@gmail.com', '9000000001', SYSDATETIME(), SYSDATETIME()),
+(102, 'Suresh Reddy', 'Chennai', 'suresh.reddy@gmail.com', '9000000002', SYSDATETIME(), SYSDATETIME()),
+(103, 'Anil Sharma', 'Bangalore', 'anil.sharma@gmail.com', '9000000003', SYSDATETIME(), SYSDATETIME()),
+(104, 'Priya Rao', 'Pune', 'priya.rao@gmail.com', '9000000004', SYSDATETIME(), SYSDATETIME()),
+(105, 'Kiran Patel', 'Mumbai', 'kiran.patel@gmail.com', '9000000005', SYSDATETIME(), SYSDATETIME());
+
+SELECT * FROM dbo.SourceCustomer;
